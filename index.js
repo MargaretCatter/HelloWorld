a4eeea4f3be924962b5f8d609f7af520f2c84f9c
Margaret Catter <30423669+MargaretCatter@users.noreply.github.com>
2022-03-12 14:15:31 -0800

    eh

---
+++ index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+// Import from a CSS file in your src
+import "../styles/index.css";
+// Import from an installed package
+// Import "bootstrap/dist/css/bootstrap.min.css"
+
+export default function HomePage() {
+	return index.mdx;
+}
